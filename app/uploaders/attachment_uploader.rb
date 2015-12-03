9cfbe04a242018fbb7d3e44eaa66285d9652a34d
brianmock <briannealmock@gmail.com>
2015-12-03 11:48:47 -0600

    Modify upload path, modify seeds

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -14,7 +14,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}"
+    "#{model.class.to_s.underscore}"
   end
 
   def cache_dir
