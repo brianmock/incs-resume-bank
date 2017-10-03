28e81a8d9325add5c14d502f841c956021bb1a2f
brianmock <briannealmock@gmail.com>
2017-10-03 11:44:57 -0500

    Update to dropbox v2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+  p ENV["ACCESS_TOKEN"]
   config.dropbox_app_key = ENV["APP_KEY"]
   config.dropbox_app_secret = ENV["APP_SECRET"]
   config.dropbox_access_token = ENV["ACCESS_TOKEN"]
