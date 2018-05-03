61caaf934737d642a77d7102b0037e9270b5d874
brianmock <briannealmock@gmail.com>
2018-05-03 17:16:16 -0500

    Add page where admin can delete certain objects

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,22 @@
+class OrganizationsController < ApplicationController
+  before_filter :authorize
+
+  def destroy
+    @org = Organization.find(params[:id])
+    @org.destroy
+    redirect_to admin_path
+  end
+
+  private 
+
+  def authorize
+    if !session[:user_id]
+      redirect_to root_path
+    end
+    if session[:user_id]
+      if User.find_by(id: session[:user_id]).access != "admin"
+        redirect_to root_path
+      end
+    end
+  end
+end
